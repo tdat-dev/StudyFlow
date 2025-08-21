dc74c040eab59be92b6cddf95f9dae9ebb8d2c69
tdat <tvmaroka1@gmail.com>
2025-08-21 13:05:32 +0700

    chore: update package dependencies to version 7.4.3 for @capacitor/android, @capacitor/cli, @capacitor/core, and @capacitor/ios

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'studyflow',
+  webDir: 'public'
+};
+
+export default config;
